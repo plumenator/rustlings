3a442f5160d26e9fb1add55f3c6dcc2c431378ab
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-05-26 17:55:15 +0800

    Solve exercises/quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,18 +14,28 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+pub enum Grade {
+    Numerical(f32),
+    Alphabetical(String),
+}
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
 
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name,
+            &self.student_age,
+            match &self.grade {
+                Grade::Numerical(f) => f.to_string(),
+                Grade::Alphabetical(s) => s.to_string(),
+            }
+        )
     }
 }
 
@@ -36,7 +46,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Numerical(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -48,9 +58,8 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Alphabetical("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
